d52d9d4324c8897b9354f2f1874b432149273aee
xarDeves <ch.nteves@gmail.com>
2022-03-01 16:45:08 +0200

    extracted method from InboundHandler

--- app/src/main/java/networker/messages/content/ContentProvider.java
+++ app/src/main/java/networker/messages/content/ContentProvider.java
@@ -1,7 +1,17 @@
 package networker.messages.content;
 
+import java.io.IOException;
+
 public interface ContentProvider<H, D> {
+    void insertData(byte[] buffer, int count, Type dt) throws IOException;
+
     H getHeader();
+
     D getData();
+
     long getTotalSize();
+
+    enum Type {
+        HEADER, BODY
+    }
 }
--- app/src/main/java/networker/messages/content/providers/TextProvider.java
+++ app/src/main/java/networker/messages/content/providers/TextProvider.java
@@ -2,6 +2,7 @@ package networker.messages.content.providers;
 
 import networker.helpers.NetworkUtilities;
 import networker.messages.content.ContentProvider;
+
 /** buffer size must remain constant and NOT change!
  *  bodyCount is the full byte size of the contents
  */
@@ -11,10 +12,11 @@ public class TextProvider implements ContentProvider<Object, String> {
     private int startingIndex = 0;
 
     public TextProvider(int bodyCount) {
-            bArray = new byte[bodyCount];
+        bArray = new byte[bodyCount];
     }
 
-    public void insertData(byte[] buffer, int count) {
+    @Override
+    public void insertData(byte[] buffer, int count, Type dt) {
         // suppose startingIndex is 0, count is 2, it will
         //  write to 0 and 1 index, and next time start from index 2
         System.arraycopy(buffer, 0, bArray, startingIndex, count);
